ed6b2d00c65d563b3e615573864fc6179fb39f87
howardc72150 <52299899+howardc72150@users.noreply.github.com>
2020-02-12 10:35:34 +1300

    Checks that number inputs are more than zero.

--- 03_find_scale_factor.py
+++ 03_find_scale_factor.py
@@ -6,13 +6,27 @@
 # Warn the user if the sf is less than 0.25 or more than 4
 
 # Functions:
+def num_check(question):
 
+    error = "Please enter a number that is more than zero."
+
+    valid = False
+    while not valid:
+        try:
+            response = float(input(question))
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+        except ValueError:
+            print(error)
 
 
 # Main routine:
 
-serving_size = float(input("What is the recipe serving size? "))
-desired_size = float(input("How many servings are needed? "))
+serving_size = num_check("What is the recipe serving size? ")
+desired_size = num_check("How many servings are needed? ")
 
 scale_factor = desired_size / serving_size
 
