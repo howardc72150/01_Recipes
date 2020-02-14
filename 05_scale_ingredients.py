f863ce80305dbe4cd6110b2221eb7a248764174f
howardc72150 <52299899+howardc72150@users.noreply.github.com>
2020-02-14 14:16:04 +1300

    Scale ingredients file

--- 05_scale_ingredients.py
+++ 05_scale_ingredients.py
@@ -7,15 +7,19 @@ def num_check(question):
 
     valid = False
     while not valid:
-        try:
-            response = float(input(question))
+        response = (input(question))
 
-            if response <= 0:
+        if response.lower() == "xxx":
+            return "xxx"
+        else:
+            try:
+                if float(response) <= 0:
+                    print(error)
+                else:
+                    return response
+
+            except ValueError:
                 print(error)
-            else:
-                return response
-        except ValueError:
-            print(error)
 
 # Not blank function goes here:
 def not_blank(question, error_msg, num_ok):
@@ -53,25 +57,23 @@ ingredients = []
 # Loop to ask user to enter an ingredient:
 stop = ""
 while stop != "xxx":
-    amount = num_check("How much? ")
-    scaled = amount * scale_factor
-
-    # Ask user for ingredient (via not blank function):
-    get_ingredient = not_blank("Please type in an ingredient name: ",
-                               "This can't be blank",
-                               "yes")
+    amount = num_check("What is the amount for the ingredient? ")
 
     # Stop looping if exit code is typed and there are more
     # than 2 ingredients:
-    if get_ingredient.lower() == "xxx" and len(ingredients) > 1:
+    if amount.lower() == "xxx" and len(ingredients) > 1:
         break
 
-    elif get_ingredient.lower() == "xxx" and len(ingredients) < 2:
+    elif amount.lower() == "xxx" and len(ingredients) < 2:
         print("You need at lesat two ingredients in the list. "
               "Please add more ingredients.")
     # If exit code is not entered, add ingregient to list:
     else:
-        ingredients.append(get_ingredient)
+        get_ingredient = not_blank("Please type in an ingredient name: ",
+                                   "This can't be blank",
+                                   "yes")
+        amount = float(amount) * scale_factor
+        ingredients.append("{} units {}".format(amount, get_ingredient))
 
 # Output list:
 print(ingredients)
