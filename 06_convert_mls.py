e5f1676f6879b967d0988205f8ada1ae8a06cd2a
howardc72150 <52299899+howardc72150@users.noreply.github.com>
2020-02-18 12:14:17 +1300

    Added multiple files.

--- 06_convert_mls.py
+++ 06_convert_mls.py
@@ -6,11 +6,43 @@
 
 # If no unit given/unit is unknown, leave as is.
 
+# Function:
+def unit_checker():
+    unit_tocheck = input("Unit? ")
+
+    # Abbreviation lists:
+    teaspoon = ["tsp", "teaspoon", "t"]
+    tablespoon = ["tbs", "tablespoon"," T"," tbsp"]
+    cup = ["c", "cups", "C"]
+    ounce = ["oz"]
+    pint = ["p"]
+    quart = ["q"]
+    pound = ["lb", "lbs"]
+
+    if unit_tocheck == "":
+        print("You chose {}".format(unit_tocheck))
+        return unit_tocheck
+    elif unit_tocheck == "T" or unit_tocheck.lower() in tablespoon:
+        return "tbs"
+    elif unit_tocheck.lower() in teaspoon:
+        return "tsp"
+    elif unit_tocheck in cup:
+        return "cup"
+    elif unit_tocheck.lower() in ounce:
+        return "ounce"
+    elif unit_tocheck.lower() in pint:
+        return "pint"
+    elif unit_tocheck.lower() in quart:
+        return "quart"
+    elif unit_tocheck.lower() in pound:
+        return "pound"
+
+# Main routine:
 unit_central = {
     "tsp": 5,
     "tbs": 15,
     "cup": 237,
-    "ounce": 30,
+    "ounce": 28.35,
     "pint": 473,
     "quart": 946,
     "pound": 454
@@ -22,12 +54,12 @@ while keep_going == "":
     amount = eval(input("How much? "))
     amount = float(amount)
 
-    unit = input("Unit? ")
+    unit = unit_checker()
 
     if unit in unit_central:
         mult_by = unit_central.get(unit)
         amount = amount * mult_by
-        print("Amount in mL {}".format(amount))
+        print("Amount in mL {:.2f}".format(amount))
     else:
         print("{} is unchanged".format(amount))
 
