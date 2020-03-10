77fe7ad3f041590568b879869860a0d0e6393df4
howardc72150 <52299899+howardc72150@users.noreply.github.com>
2020-03-11 10:45:42 +1300

    Added convert v2 and fixed bugs with the main program.

--- 10_recipe_moderniser.py
+++ 10_recipe_moderniser.py
@@ -2,15 +2,6 @@
 import csv
 import re
 
-full_recipe =[
-    "1 1/2 ml flour",
-    "3/4 cup milk",
-    "1 cup flour",
-    "2 tablespoons white sugar",
-    "1.5 tsp baking powder",
-    "pinch of cinnamon"
-]
-
 # Functions:
 
 def not_blank(question, error_msg, num_ok):
@@ -102,8 +93,85 @@ def get_all_ingredients():
         else:
             all_ingredients.append(get_recipe_line)
 
+    return all_ingredients
+
+def general_converter(how_much, lookup, dictionary, conversion_factor):
+    if lookup in dictionary:
+        mult_by = dictionary.get(lookup)
+        how_much = how_much * mult_by * conversion_factor
+        converted = "yes"
+    else:
+        converted = "no"
+    return [how_much, converted]
+
+def unit_checker():
+    unit_tocheck = input("Unit? ")
+
+    # Abbreviation lists:
+    teaspoon = ["tsp", "teaspoon", "t", "teaspoons"]
+    tablespoon = ["tbs", "tablespoon"," T"," tbsp", "tablespoons"]
+    cup = ["c", "cups", "cup"]
+    ounce = ["oz", "ounce", "fl oz", "ounces"]
+    pint = ["p", "pt", "fl pt", "pint", "pints"]
+    quart = ["q", "qt", "fl qt", "quart", "quarts"]
+    pound = ["lb", "pound", "#", "pounds"]
+    mls = ["ml", "milliliter", "millilitre", "milliliters", "millilitres"]
+    litre = ["litre", "liter", "l", "litres", "liters"]
+    grams = ["g", "gram", "grams"]
+
+    if unit_tocheck == "":
+       #print("You chose {}".format(unit_tocheck))
+        return unit_tocheck
+    elif unit_tocheck == "T" or unit_tocheck.lower() in tablespoon:
+        return "tbs"
+    elif unit_tocheck.lower() in teaspoon:
+        return "tsp"
+    elif unit_tocheck in cup:
+        return "cup"
+    elif unit_tocheck.lower() in ounce:
+        return "ounce"
+    elif unit_tocheck.lower() in pint:
+        return "pint"
+    elif unit_tocheck.lower() in quart:
+        return "quart"
+    elif unit_tocheck.lower() in pound:
+        return "pound"
+    elif unit_checker.lower() in litre:
+        return "litre"
+    elif unit_checker.lower() in mls:
+        return "ml"
+    elif unit_checker.lower() in grams:
+        return "g"
 
 # Main routine:
+unit_central = {
+    "tsp": 5,
+    "tbs": 15,
+    "cup": 237,
+    "ounce": 28.35,
+    "pint": 473,
+    "quart": 946,
+    "pound": 454,
+    "litre": 1000,
+    "ml": 1
+}
+
+# Open file:
+groceries = open('01_ingredients_ml_to_g.csv')
+
+# Read data into a list:
+csv_groceries = csv.reader(groceries)
+
+# Create a dictionary to hold the data:
+food_dictionary = {}
+
+# Add the data from the list into the dictionary
+# (first item in row is key, second is definition)
+
+for row in csv_groceries:
+    food_dictionary[row[0]] = row[1]
+
+# print(food_dictionary)
 
 # Set up dictionaries:
 
@@ -142,6 +210,7 @@ for recipe_line in full_recipe:
         amount = mixed_num.replace(" ", "+")
         # Change the string into a decimal
         amount = eval(amount)
+        amount = amount * scale_factor
 
         # Get unit and ingredient
         compile_regex = re.compile(mixed_regex)
@@ -166,11 +235,15 @@ for recipe_line in full_recipe:
     unit = get_unit[0]
     # Convert into ml
 
-    ingredient = get_unit[1]
-    # Convert into g
+    num_spaces = recipe_line.count(" ")
+    if num_spaces > 1:
+        ingredient = get_unit[1]
+        # Convert into g
+    else:
+        modernised_recipe.append("{} {}".format(amount, unit_ingredient))
+        continue
 
     modernised_recipe.append("{} {} {}".format(amount, unit, ingredient))
-
 # Put updated ingredient in list
 
 # Output ingredient list
